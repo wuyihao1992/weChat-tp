7d712c35034a03a359f4c01f75fee35717ba1242
wuyihao1992 <840461198@qq.com>
2017-05-10 18:25:46 +0800

    [add] gulp css sprity

--- Public/gulpfile.js
+++ Public/gulpfile.js
@@ -12,11 +12,16 @@ var fs = require('fs'),
     rename = require('gulp-rename'),
     zip = require('gulp-zip'),
     del = require('del');
+
 var runSequence = require('run-sequence').use(gulp);
 var argv = require('yargs').argv;
 var browserSync = require('browser-sync').create();
 var gulpif = require('gulp-if');
-var sprite = require('css-sprite').stream;
+
+// var sprite = require('css-sprite').stream;
+var sprite = require('sprity');
+var spriter = require('gulp-css-spriter');
+var spritesmith = require('gulp.spritesmith');
 
 var option = {
     read : false,
@@ -156,10 +161,10 @@ gulp.task('js', function() {
 });
 
 /**
- * css sprite 生成雪碧图（CSS图像拼合技术，CSS贴图定位）
+ * css-sprite 生成雪碧图（CSS图像拼合技术，CSS贴图定位）
  */
 // generate sprite.png and _sprite.scss
-gulp.task('sprites', function () {
+gulp.task('sprite', function () {
   return gulp.src('./src/img/*.png')
     .pipe(sprite({
       name: 'sprite',
@@ -171,7 +176,7 @@ gulp.task('sprites', function () {
 });
 // generate scss with base64 encoded images
 gulp.task('base64', function () {
-  return gulp.src('./src/img/*.png')
+  return gulp.src('./img/*.png')
     .pipe(sprite({
       base64: true,
       style: '_base64.scss',
@@ -180,7 +185,66 @@ gulp.task('base64', function () {
     .pipe(gulp.dest('./dist/scss/'));
 });
 
+/**
+ * gulp-css-spriter 生成雪碧图（CSS图像拼合技术，CSS贴图定位）
+ */
+gulp.task('cssSpriter', function() {
+    return gulp.src('./css/recharge.css')//比如recharge.css这个样式里面什么都不用改，是你想要合并的图就要引用这个样式。 很重要 注意(recharge.css)这个是我的项目。别傻到家抄我一样的。
+        .pipe(spriter({
+            // The path and file name of where we will save the sprite sheet
+            'spriteSheet': './dist/images/spritesheet.png', //这是雪碧图自动合成的图。 很重要
+            // Because we don't know where you will end up saving the CSS file at this point in the pipe,
+            // we need a litle help identifying where it will be.
+            'pathToSpriteSheetFromCSS': '../images/spritesheet.png' //这是在css引用的图片路径，很重要
+        }))
+        .pipe(gulp.dest('./dist/css')); //最后生成出来
+});
 
+/**
+ * gulp.spritesmith 生成雪碧图（CSS图像拼合技术，CSS贴图定位）
+ */
+gulp.task('spritesmith', function () {
+    return gulp.src('./img/*.png') //需要合并的图片地址
+        .pipe(spritesmith({
+            imgName: 'sprite.png', //保存合并后图片的地址
+            cssName: 'css/sprite.css', //保存合并后对于css样式的地址
+            padding: 5, //合并时两个图片的间距
+            algorithm: 'binary-tree',//Algorithm 有四个可选值分别为top-down、left-right、diagonal、alt-diagonal、binary-tree
+            cssTemplate:"css/handlebarsStr.css"//cssTemplate 是生成css的模板文件可以是字符串也可以是函数.字符串是对于相对于的模板地址,模板文件样式格式如下
+        }))
+        .pipe(gulp.dest('dist/'));
+});
+/**
+cssTemplate: css/handlebarsStr.css模板文件样式格式是
+
+{{#sprites}}
+.icon-{{name}}{
+    background-image: url("{{escaped_image}}");
+    background-position: {{px.offset_x}} {{px.offset_y}};
+    width: {{px.width}};
+    height: {{px.height}};
+}
+{{/sprites}}
+
+cssTemplate:对于函数样式格式是
+cssTemplate: function (data) {
+    var arr=[];
+    data.sprites.forEach(function (sprite) {
+        arr.push(".icon-"+sprite.name+
+        "{" +
+        "background-image: url('"+sprite.escaped_image+"');"+
+        "background-position: "+sprite.px.offset_x+"px "+sprite.px.offset_y+"px;"+
+        "width:"+sprite.px.width+";"+
+        "height:"+sprite.px.height+";"+
+        "}\n");
+    });
+    return arr.join("");
+}
+ */
+
+/**
+ * 监听任务
+ */
 gulp.task('watch', ['clean', 'sass'], function() {
     gulp.watch('./sass/**/*.scss', ['sass']);
 });
@@ -188,7 +252,7 @@ gulp.task('watch', ['clean', 'sass'], function() {
 gulp.task('default', ['watch']);
 
 /**
- * function回调，用于配置异步任务。避免边删除边编译的情况。
+ * function(cb)回调，用于配置异步任务。避免边删除边编译的情况。
  */
 gulp.task('build', ['clean'], function(cb) {
     return gulp.start(['framework', 'sass'], cb);
